765ccd3e8511d47a8969d0d913fbe922bae597b7
Liu Haibin <liu.haibin@gmail.com>
2018-03-15 08:07:57 +0800

    Add attributes to reset

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+node.default['ipaddress'] = '1.1.1.1'
+node.default['motd-attributes']['company'] = 'My Company'
 node.default['motd-attributes']['message'] = "It's a wonderful day today!"
 
 template '/etc/motd' do
